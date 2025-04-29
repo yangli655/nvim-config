82a8d130ae4cdf938c78f6422c0adbb4e2c9e41c
liyang <liyang.ly@hotmail.com>
2025-04-29 16:07:36 +0800

    update lazy.nvim config

--- lua/lazy-nvim.lua
+++ lua/lazy-nvim.lua
@@ -5,6 +5,15 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -195,20 +204,13 @@ require("lazy").setup({
 		"mhartington/formatter.nvim",
 		lazy = true,
 	},
-	pkg = {
-		enabled = true,
-		cache = vim.fn.stdpath("state") .. "/lazy/pkg-cache.lua",
-		versions = true, -- Honor versions in pkg sources
-		-- the first package source that is found for a plugin will be used.
-		sources = {
-			"lazy",
-			"packspec",
-		},
-	},
 	rocks = {
 		enabled = false,
 		-- hererocks = false,
 	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
 	-- automatically check for plugin updates
 	checker = { enabled = true },
 })
