76edd5cf294f29382410cf02f56c072faaf90b80
liyang <liyang.ly@hotmail.com>
2025-12-02 19:34:45 +0800

    feat: add plugin kylechui/nvim-surround

--- lua/lazy-nvim.lua
+++ lua/lazy-nvim.lua
@@ -80,9 +80,23 @@ require("lazy").setup({
 	},
 
 	{
-		"delphinus/auto-cursorline.nvim",
+		"kylechui/nvim-surround",
+		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
 		config = function()
-			require("auto-cursorline").setup({})
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+				--     Old text                    Command         New text
+				-- --------------------------------------------------------------------------------
+				--     surr*ound_words             ysiw)           (surround_words)
+				--     surr*ound_words             ysiw(           ( surround_words )
+				--     *make strings               ys$"            "make strings"
+				--     [delete ar*ound me!]        ds]             delete around me!
+				--     remove <b>HTML t*ags</b>    dst             remove HTML tags
+				--     'change quot*es'            cs'"            "change quotes"
+				--     <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+				--     delete(functi*on calls)     dsf             function calls
+			})
 		end,
 	},
 
